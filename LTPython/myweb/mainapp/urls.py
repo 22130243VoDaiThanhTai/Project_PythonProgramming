e6b56454fce42a1fdef4df425cfc4ebe48cc2ffc
Tan <22130246@st.hcmuaf.edu.vn>
2025-12-03 17:28:09 +0700

    ket noi db va chay trang home, detail

--- LTPython/myweb/mainapp/urls.py
+++ LTPython/myweb/mainapp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('policy/', views.policy, name='policy'),
     path('terms/', views.terms, name='terms'),
-    
+    path('product/<int:product_id>/', views.detail, name='detail'),
 ]
