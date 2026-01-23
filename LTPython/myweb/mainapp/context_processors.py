550333d1d5940e7db96936725367855a1d8da51c
viettein <119783901+viettein@users.noreply.github.com>
2026-01-23 13:16:13 +0700

    Revert "Add custom admin login and dashboard UI"

--- LTPython/myweb/mainapp/context_processors.py
+++
@@ -1,7 +0,0 @@
-from .models import Category
-
-def categories_processor(request):
-    """Make categories available in all templates"""
-    return {
-        'listCategory': Category.objects.all()
-    }
